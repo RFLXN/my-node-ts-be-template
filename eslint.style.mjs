ef3eb62826409c7e0f90398ad5e8618e65be8230
J.H. CHOI | RFLXN <54879477+RFLXN@users.noreply.github.com>
2024-08-23 20:06:45 +0900

    Update eslint.style.mjs

--- eslint.style.mjs
+++ eslint.style.mjs
@@ -2,6 +2,7 @@ import stylistic from "@stylistic/eslint-plugin";
 
 export default [
     stylistic.configs.customize({
+        braceStyle: "1tbs",
         indent: 4,
         quotes: "double",
         semi: true,
